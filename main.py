823c54c23b7737b02212d975029812a5e73dbef5
Faux Tech <mgmarcin.gontarek@gmail.com>
2021-01-27 19:39:04 +0100

    add skill reroll

--- main.py
+++ main.py
@@ -2,24 +2,29 @@
 class BloodBowlCalculator:
 
     def __init__(self):
-        self.probability = 1
+        pass
 
-    def prob_calc_reroll(self, rolls, rerolls=1):
+    def prob_calc(self, rolls, reroll_skill, reroll=1, repeat=1):
         if not rolls:
             return 1
-        probability = (7 - rolls[0]) / 6 * self.prob_calc_reroll(rolls[1:], rerolls)
-        if rerolls == 1:
-            probability += (rolls[0] - 1) / 6 * self.prob_calc_reroll(rolls, 0)
+
+        # successful rolls
+        probability = (7 - rolls[0][0]) / 6 * self.prob_calc(rolls[1:], reroll_skill, reroll, repeat=1)
+
+        # failed rolls being repeated with team/skill reroll
+        if reroll_skill[rolls[0][1]] == 1 and repeat == 1:
+            new_reroll_skill = dict(reroll_skill)
+            new_reroll_skill[rolls[0][1]] = 0
+            probability += (rolls[0][0] - 1) / 6 * self.prob_calc(rolls, new_reroll_skill, reroll, repeat=0)
+        elif reroll == 1 and repeat == 1:
+            probability += (rolls[0][0] - 1) / 6 * self.prob_calc(rolls, reroll_skill, 0, repeat=0)
+
         return probability
 
 
 if __name__ == '__main__':
+    skills = {'catch': 1, 'dodge': 1, 'pass_skill': 1, 'sure_feet': 1, 'sure_hands': 1, 'reroll': 1}
+    roll = [[2, 'dodge'], [3, 'catch'], [2, 'dodge']]
+
     calculator = BloodBowlCalculator()
-    print(83.333, calculator.prob_calc_reroll([2], 0))
-    print(97.222, calculator.prob_calc_reroll([2]))
-    print(41.667, calculator.prob_calc_reroll([2, 4], 0))
-    print(69.444, calculator.prob_calc_reroll([2, 4]))
-    print(6.944, calculator.prob_calc_reroll([2, 4, 6], 0))
-    print(17.361, calculator.prob_calc_reroll([2, 4, 6]))
-    print(1.543, calculator.prob_calc_reroll([2, 4, 6, 5, 3], 0))
-    print(5.401, calculator.prob_calc_reroll([2, 4, 6, 5, 3]))
+    print(calculator.prob_calc(roll, skills, reroll=1))
