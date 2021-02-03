e274924339eb70a03a7ed41007c53cb84f013e43
Faux Tech <mgmarcin.gontarek@gmail.com>
2021-02-03 11:24:10 +0100

    integrate -2d/-3d block into the main function

--- main.py
+++ main.py
@@ -8,19 +8,18 @@ class BloodBowlCalculator:
         if not rolls:
             return 1
 
-        # successful rolls
-        # put calculations into functions and create variations for different situations like -2d
+        # successful roll calculation
         probability = self.roll_success(rolls[0][0], rolls[0][1]) *\
             self.prob_calc(rolls[1:], reroll_skill, reroll, repeat=1)
 
-        # failed rolls being repeated with team/skill reroll
+        # skill reroll calculation
         if reroll_skill[rolls[0][2]] == 1 and repeat == 1:
             new_reroll_skill = dict(reroll_skill)
             new_reroll_skill[rolls[0][2]] = 0
             probability += self.roll_fail(rolls[0][0], rolls[0][1]) * \
                 self.prob_calc(rolls, new_reroll_skill, reroll, repeat=0)
+        # team reroll calculation
         elif reroll == 1 and repeat == 1:
-            # -2/3 dice need to subtract prob instead of adding
             probability += self.roll_fail(rolls[0][0], rolls[0][1]) * \
                 self.prob_calc(rolls, reroll_skill, 0, repeat=0)
 
@@ -28,18 +27,26 @@ class BloodBowlCalculator:
 
     @staticmethod
     def roll_success(success, num_dice):
+        # -2d/3d block variant
+        if num_dice < 0:
+            return pow(success / 6, abs(num_dice))
+
         return 1 - pow(1 - success / 6, num_dice)
 
     @staticmethod
-    def roll_fail(fail, num_dice):
-        return pow(1 - fail / 6, num_dice)
+    def roll_fail(success, num_dice):
+        # -2d/3d block variant
+        if num_dice < 0:
+            return 1 - pow(success / 6, abs(num_dice))
 
-    @staticmethod
-    def minus_block_prototype(success, num_dice):
-        prob_no_reroll = pow(success / 6, num_dice)
-        prob_reroll = pow(success / 6, num_dice) + \
-            (1 - pow(success / 6, num_dice)) * pow(success / 6, num_dice)
-        return prob_no_reroll * 100, prob_reroll * 100
+        return pow(1 - success / 6, num_dice)
+
+    # @staticmethod
+    # def minus_block(success, num_dice):
+    #     prob_no_reroll = pow(success / 6, num_dice)
+    #     prob_reroll = pow(success / 6, num_dice) + \
+    #         (1 - pow(success / 6, num_dice)) * pow(success / 6, num_dice)
+    #     return prob_no_reroll * 100, prob_reroll * 100
 
 
 if __name__ == '__main__':
@@ -47,8 +54,7 @@ if __name__ == '__main__':
     # roll = [[5, 1, 'dodge'], [4, 1, 'catch'], [5, 1, 'dodge']]
     # roll = [[5, 1, 'dodge'], [5, 1, 'catch'], [5, 1, 'dodge'], [5, 2, 'block'],
     #         [5, 1, 'pass_skill'], [5, 1, 'sure_feet'], [5, 1, 'sure_hands']]
-    # roll = [[1, 3, 'block'], [5, 1, 'dodge'], [2, 1, 'dodge'], [3, 1, 'catch']]
-    # block = [[2, 2]]
+    # roll = [[1, -2, 'block'], [5, 1, 'dodge'], [2, 1, 'dodge'], [3, 1, 'catch']]
+    roll = [[5, -2, 'block'], [5, 1, 'dodge'], [5, 1, 'dodge'], [5, 1, 'dodge'], [4, 1, 'dodge']]
     calculator = BloodBowlCalculator()
-    # print(calculator.prob_calc(roll, skills, reroll=1) * 100)
-    print(calculator.minus_block_prototype(5, 2))
+    print(calculator.prob_calc(roll, skills, reroll=1) * 100)
